cd6fc77046324e5d6f2dfc3d3b0dfb68e788f39a
Tyranowski <andretyranowski@gmail.com>
2024-07-21 15:10:34 +0200

    removed unused method and dom from script.js

--- javascript/script.js
+++ javascript/script.js
@@ -2,25 +2,18 @@ import MindMap from "./mindmap/MindMap.js";
 
 const mindMap = new MindMap("mindMapCanvas");
 
-// Get button elements by their IDs
 const undoButton = document.getElementById("undo-button");
 const redoButton = document.getElementById("redo-button");
 const saveButton = document.getElementById("save-button");
 const loadButton = document.getElementById("import-button");
 const exportButton = document.getElementById("export-button");
-const colorButton = document.getElementById("color-button");
-const resizeButton = document.getElementById("resize-button");
 
-// Add event listeners to each button
 undoButton.addEventListener("click", handleUndo);
 redoButton.addEventListener("click", handleRedo);
 saveButton.addEventListener("click", handleSave);
 loadButton.addEventListener("click", handleImport);
 exportButton.addEventListener("click", handleExport);
-colorButton.addEventListener("click", handleColor);
-resizeButton.addEventListener("click", handleResize);
 
-// Define the button click handler functions
 function handleUndo() {
   mindMap.circleController.undo();
 }
