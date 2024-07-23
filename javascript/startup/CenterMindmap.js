1f96d4f2a0ca53434340d6a8693c8e85f26e602c
Tyranowski <andretyranowski@gmail.com>
2024-07-23 14:10:46 +0200

    added scroll to center functionnality for ctrl + o

--- javascript/startup/CenterMindmap.js
+++ javascript/startup/CenterMindmap.js
@@ -1,28 +1,32 @@
 const canvasContainer = document.querySelector("#canvas-container");
 
+const CanvasScroller = {
+  scrollToCenter() {
+    const { scrollWidth, scrollHeight, clientWidth, clientHeight } =
+      canvasContainer;
+
+    const centerX = this.calculateCenterX(scrollWidth, clientWidth);
+    const centerY = this.calculateCenterY(scrollHeight, clientHeight);
+
+    this.setScrollPosition(centerX, centerY);
+  },
+
+  calculateCenterX(scrollWidth, clientWidth) {
+    return (scrollWidth - clientWidth) / 2;
+  },
+
+  calculateCenterY(scrollHeight, clientHeight) {
+    return (scrollHeight - clientHeight) / 2;
+  },
+
+  setScrollPosition(centerX, centerY) {
+    canvasContainer.scrollLeft = centerX;
+    canvasContainer.scrollTop = centerY;
+  },
+};
+
 document.addEventListener("DOMContentLoaded", function () {
-  scrollToCenter();
+  CanvasScroller.scrollToCenter();
 });
 
-function scrollToCenter() {
-  const { scrollWidth, scrollHeight, clientWidth, clientHeight } =
-    canvasContainer;
-
-  const centerX = calculateCenterX(scrollWidth, clientWidth);
-  const centerY = calculateCenterY(scrollHeight, clientHeight);
-
-  setScrollPosition(centerX, centerY);
-}
-
-function calculateCenterX(scrollWidth, clientWidth) {
-  return (scrollWidth - clientWidth) / 2;
-}
-
-function calculateCenterY(scrollHeight, clientHeight) {
-  return (scrollHeight - clientHeight) / 2;
-}
-
-function setScrollPosition(centerX, centerY) {
-  canvasContainer.scrollLeft = centerX;
-  canvasContainer.scrollTop = centerY;
-}
+export default CanvasScroller;
