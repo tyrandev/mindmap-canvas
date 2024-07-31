ba42364c5bdc16a11f2f3b31bad15384701eb2ab
Tyranowski <andretyranowski@gmail.com>
2024-07-31 08:41:49 +0200

    fixed upper-menu height

--- js/util/canvas/CenterUtil.js
+++ js/util/canvas/CenterUtil.js
@@ -4,7 +4,6 @@ export default class CenterUtil {
     if (!element) {
       throw new Error(`Element with id "${elementId}" not found.`);
     }
-
     const rect = element.getBoundingClientRect();
     return rect.width / 2;
   }
@@ -14,7 +13,6 @@ export default class CenterUtil {
     if (!element) {
       throw new Error(`Element with id "${elementId}" not found.`);
     }
-
     const rect = element.getBoundingClientRect();
     return rect.height / 2;
   }
