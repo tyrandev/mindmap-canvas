0576e06e01a8c79f3a6d8baf71cedda976128878
Tyranowski <andretyranowski@gmail.com>
2024-07-31 09:30:32 +0200

    updated rectangle

--- js/model/geometric/rectangle/RectangleConstants.js
+++ js/model/geometric/rectangle/RectangleConstants.js
@@ -8,4 +8,4 @@ export const DEFAULT_WIDTH_INCREMENT = 2;
 export const DEFAULT_HEIGHT_INCREMENT = 1;
 export const NODE_DEFAULT_NAME = "New Node";
 export const RECTANGLE_MAX_CHARACTERS = 25;
-export const PIXELS_PER_CHARACTER = 11;
+export const PIXELS_PER_CHARACTER = 9.5;
