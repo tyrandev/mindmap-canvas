cb311028a646163c6c4b6fca2f17ef858516f70a
Tyranowski <andretyranowski@gmail.com>
2024-08-05 22:27:18 +0200

    refactored mindmap class

--- js/model/mindmap/MindMap.js
+++ js/model/mindmap/MindMap.js
@@ -9,13 +9,29 @@ import * as GlobalConstants from "../../constants/GlobalConstants.js";
 
 export default class MindMap {
   constructor() {
+    this.initializeCanvas();
+    this.initializeControllers();
+    this.initializeHandlers();
+    this.initializeUtilities();
+  }
+
+  initializeCanvas() {
     this.canvas = document.getElementById(GlobalConstants.MINDMAP_CANVAS_ID);
     this.context = this.canvas.getContext("2d");
+  }
+
+  initializeControllers() {
     this.nodeController = new NodeController(this.canvas, this.context);
     this.fileHandler = new LocalStorageFileHandler(this.nodeController);
+  }
+
+  initializeHandlers() {
     this.keyboardHandler = new KeyboardHandler(this);
     this.mouseHandler = new MouseHandler(this);
-    this.TopMenuHandler = new TopMenuHandler(this);
+    this.topMenuHandler = new TopMenuHandler(this);
+  }
+
+  initializeUtilities() {
     ScrollUtil.scrollToCenter();
     DraggingModeUtil.init();
   }
