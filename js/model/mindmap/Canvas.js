68efd8b7d0e1f601f29c28ccb977547fae2d58dc
Tyranowski <andretyranowski@gmail.com>
2024-08-13 01:09:50 +0200

    added class Canvas for handling canvas related operations

---
+++ js/model/mindmap/Canvas.js
@@ -0,0 +1,53 @@
+import * as GlobalConstants from "../../constants/GlobalConstants.js";
+
+class Canvas {
+  constructor() {
+    if (Canvas.instance) {
+      return Canvas.instance;
+    }
+
+    this.canvas = null;
+    this.context = null;
+    Canvas.instance = this;
+    return this;
+  }
+
+  initializeCanvas() {
+    if (!this.canvas) {
+      this.canvas = document.getElementById(GlobalConstants.MINDMAP_CANVAS_ID);
+      this.context = this.canvas.getContext("2d");
+    }
+  }
+
+  setCanvasSize(width, height) {
+    if (!this.canvas) {
+      console.error("Canvas has not been initialized.");
+      return;
+    }
+
+    const ratio = window.devicePixelRatio || 1;
+    const style = this.canvas.style;
+    style.width = `${width}px`;
+    style.height = `${height}px`;
+    this.canvas.width = width * ratio;
+    this.canvas.height = height * ratio;
+    this.context.scale(ratio, ratio);
+  }
+
+  getCanvas() {
+    if (!this.canvas) {
+      console.error("Canvas has not been initialized.");
+    }
+    return this.canvas;
+  }
+
+  getContext() {
+    if (!this.context) {
+      console.error("Canvas context has not been initialized.");
+    }
+    return this.context;
+  }
+}
+
+const instance = new Canvas();
+export default instance;
--- js/model/mindmap/MindMap.js
+++ js/model/mindmap/MindMap.js
@@ -5,24 +5,12 @@ import MouseHandler from "../../gui/mouse/MouseHandler.js";
 import TopMenuHandler from "../../gui/topmenu/TopMenuHandler.js";
 import ScrollUtil from "../../util/canvas/ScrollUtil.js";
 import DraggingModeUtil from "../../util/canvas/DraggingModeUtil.js";
-import * as GlobalConstants from "../../constants/GlobalConstants.js";
+import Canvas from "./Canvas.js";
 
 export default class MindMap {
-  //TODO: create a singleton object Canvas
-  setCanvasSize(canvas, width, height) {
-    const ratio = window.devicePixelRatio || 1;
-    const style = canvas.style;
-    style.width = `${width}px`;
-    style.height = `${height}px`;
-    canvas.width = width * ratio;
-    canvas.height = height * ratio;
-    this.context.scale(ratio, ratio);
-  }
-
   initializeCanvas() {
-    this.canvas = document.getElementById(GlobalConstants.MINDMAP_CANVAS_ID);
-    this.context = this.canvas.getContext("2d");
-    this.setCanvasSize(this.canvas, 4000, 2160);
+    Canvas.initializeCanvas();
+    Canvas.setCanvasSize(4000, 2160);
   }
 
   initializeControllers() {
