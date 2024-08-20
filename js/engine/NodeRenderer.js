d8de33750d2621606f009b2b02ced588b9739ca1
Tyranowski <andretyranowski@gmail.com>
2024-08-20 19:43:04 +0200

    updated rendering logic
    it does not render the same node few times

--- js/engine/NodeRenderer.js
+++ js/engine/NodeRenderer.js
@@ -4,9 +4,26 @@ export default class NodeRenderer {
   constructor(nodeContainer) {
     this.nodeContainer = nodeContainer;
     this.context = Canvas.getContext();
+    this.renderedNodes = new Set();
   }
 
   drawNodes() {
-    this.nodeContainer.getNodes().forEach((node) => node.render(this.context));
+    this.renderedNodes.clear(); // Clear the set of previously rendered nodes
+    this.nodeContainer.getNodes().forEach((node) => this.renderNode(node));
+  }
+
+  renderNode(node) {
+    if (this.renderedNodes.has(node.id)) {
+      return;
+    }
+
+    // Render the node and track it
+    node.render(this.context);
+    this.renderedNodes.add(node.id);
+
+    // Recursively render children if the node has any
+    if (node.children) {
+      node.children.forEach((child) => this.renderNode(child));
+    }
   }
 }
